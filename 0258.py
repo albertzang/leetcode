d133b8b9ee98e1d928b95dfe028bee737f60c00a
Albert Zang <albertzang.ys@gmail.com>
2018-02-07 23:52:59 -0800

    add digits

---
+++ 0258.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        res = num % 9
+        return res if res else 9
