7900e9f3f96d2f6d530226d81fe36245a4f1711a
Albert Zang <albertzang.ys@gmail.com>
2018-02-10 11:45:36 -0800

    plus one

---
+++ 0066.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
+        i = n - 1
+        while i >= 0:
+            if digits[i] == 9:
+                digits[i] = 0
+                i -= 1
+            else:
+                digits[i] += 1
+                break
+        if i == -1:
+            return [1] + digits
+        else:
+            return digits
