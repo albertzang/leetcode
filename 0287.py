de8178c16c611499fc9d8a2998da611907c124ef
Albert Zang <albertzang.ys@gmail.com>
2018-04-15 15:29:03 -0700

    find the duplicate number

---
+++ 0287.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        fast = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
